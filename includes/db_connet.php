12ec058878e225dbe70ab21b3992d30bfbb5eb26
Heinth <88323089+Heinth@users.noreply.github.com>
2023-11-23 15:17:07 +0630

    Update db_connet.php

--- includes/db_connet.php
+++ includes/db_connet.php
@@ -9,4 +9,4 @@ $conn = mysqli_connect($dbhost, $username, $password, $dbname);
 if (!$conn) {
     echo "connection error" . mysqli_connect_error();
 }
-##Please change the $dbhost to name of the database container name that from docker-compose.yaml###
+##Please change the $dbhost to name of the database service name that from docker-compose.yaml###
